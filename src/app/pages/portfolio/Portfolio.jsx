002bdb458b72203e9284d45ba765b6f7975cebe9
Lawrence Pan <lawrencefeipan@gmail.com>
2016-03-25 13:17:30 -0400

    add portfolio and resume template

---
+++ src/app/pages/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+    </div>
+  );
+}
+
+export default Portfolio;
--- src/app/routes.js
+++ src/app/routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route, IndexRoute, Redirect } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './App';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
+import Portfolio from './pages/portfolio/Portfolio';
+import Resume from './pages/resume/Resume';
 
 const routes = (
   <Route path="/" component={App} >
     <IndexRoute component={Home} />
-    <Route path="about" component={About} />
+    <Route path="About" component={About} />
+    <Route path="Portfolio" component={Portfolio} />
+    <Route path="Resume" component={Resume} />
   </Route>
 );
 
